4a9e58cddcf4bd7ad00c63e5d4ef2abeb91570f6
moisme <auwwalsalaudeen@gmail.com>
2025-05-14 00:01:44 +0100

    Initial commit, this is the creation of the project

---
+++ target/debug/deps/cli_gen-450354020a42a48d.d
@@ -0,0 +1,5 @@
+/Users/mohammadsalaudeen/projects/cli_gen/target/debug/deps/libcli_gen-450354020a42a48d.rmeta: src/main.rs
+
+/Users/mohammadsalaudeen/projects/cli_gen/target/debug/deps/cli_gen-450354020a42a48d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/cli_gen-48f8d90a26d07aa1.d
@@ -0,0 +1,5 @@
+/Users/mohammadsalaudeen/projects/cli_gen/target/debug/deps/libcli_gen-48f8d90a26d07aa1.rmeta: src/main.rs
+
+/Users/mohammadsalaudeen/projects/cli_gen/target/debug/deps/cli_gen-48f8d90a26d07aa1.d: src/main.rs
+
+src/main.rs:
